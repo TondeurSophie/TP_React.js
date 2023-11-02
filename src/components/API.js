5b6cca717f0197c4336b9f3abad8e11b7a26de67
TondeurSophie <149052701+TondeurSophie@users.noreply.github.com>
2023-11-02 12:12:48 +0100

    Mise en place localStorgage dans API

--- src/components/API.js
+++ src/components/API.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import '../Styles/API.css';
 
 export default function Pokemon() {
   //Pour stocker la liste de pokemon
   const [pokemonList, setPokemonList] = useState([]);
+  
   //Requête au niveau de l'API Pokémon
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=10").then(reponse => reponse.json()).then(data => {
       //Met à jour la liste des pokemon
     setPokemonList(data.results);
       })  }, []);
+
 
   return (
     <div>
@@ -20,8 +23,9 @@ export default function Pokemon() {
   );
 }
 //Stockage des pokemon
-function PokemonDetail({ url }) {
+function PokemonDetail({ url }, pokemonList) {
   const [pokemon, setPokemon] = useState(null);
+  const ls = localStorage;
 
   useEffect(() => {
     fetch(url)
@@ -33,13 +37,31 @@ function PokemonDetail({ url }) {
     return <div>Chargement en cours...</div>;
   }
 
+  //permet d'ajouter des tâches dans la liste de l'utilisateur
+  const ajouter = () => {
+        
+    const ls = localStorage; //sauvegarde les valeurs du Local Storage
+     ls.setItem(pokemonList, "value")//reprend la valeur inscrit dans l'input pour l'ajouter dans la localStorage
+     window.location.reload()
+ }
+
   return (
-    <div>
-      <h1>{pokemon.name}</h1>
-      <p>Numéro: {pokemon.id}</p>
-      {/* Si plusieurs type affiche tous les types du pokemon à partir du tableau */}
-      <p>Type(s): {pokemon.types.map(type => type.type.name).join(', ')}</p>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+    <div className='page'>
+        <br/>
+        <span><b>Liste des Pokémons</b></span>
+        {Object.keys(ls).map(pokemonList => (
+            <div className='case'>
+                <p pokemonList={pokemonList}>
+                <h1>{pokemon.name}</h1>
+                <p>Numéro: {pokemon.id}</p>
+                {/* Si plusieurs type affiche tous les types du pokemon à partir du tableau */}
+                <p>Type(s): {pokemon.types.map(type => type.type.name).join(', ')}</p>
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                <br/>
+                <button className="bouton_add" onClick={ajouter} >Ajouter au Pokédex</button>
+                </p>
+            </div>
+        ))}
     </div>
   );
 }
--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import API from './API'
-import Ajouter from './Ajouter.js'
 
 export default function Accueil() {
   return (
     <div className='page'>
-        {/* <API/> */}
-        <Ajouter/>
-        {/* <API/> */}
+        <API/>
+        
         <br/>
     </div>
   )
