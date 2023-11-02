8b3e89cb7b48242f79f4e744e63cdaa99dc8d9be
Prapaline <124569549+Prapaline@users.noreply.github.com>
2023-11-02 15:47:08 +0100

    Add files via upload

--- API.js
+++ API.js
@@ -1,66 +1,77 @@
 import React, { useEffect, useState } from 'react';
-import '../Styles/API.css';
-
 export default function Pokemon() {
-  //Pour stocker la liste de pokemon
-  const [pokemonList, setPokemonList] = useState([]);
-  
-  //Requête au niveau de l'API Pokémon
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=10").then(reponse => reponse.json()).then(data => {
-      //Met à jour la liste des pokemon
-    setPokemonList(data.results);
-      })  }, []);
+  // Pour stocker la liste de Pokémon
+  const [Liste, setListe] = useState([]); 
+  // Pour gérer la pagination
+  const [page, setPage] = useState(1); 
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    //Chargement des pokemon par page avec limite de 5 pokemon par page
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=5&offset=${(page - 1) * 5}`).then(reponse => reponse.json()).then(data => {
+        setListe(data.results);
+        setLoading(false);
+      })
+      .catch(error => console.error(error));
+  }, [page]);
+  //augmente page de 1
+  const PageSuivante = () => {
+    setPage(page + 1);
+  };
+  //Diminue page de 1 si page est supèrieur à 1
+  const PagePrecedente = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
 
   return (
     <div>
-      {/* Parcours dans le tableau de Pokemon pour afficher l'image et le nom de chaque */}
-      {pokemonList.map(pokemon => (
-        <PokemonDetail key={pokemon.name} url={pokemon.url} />
-      ))}
+      {loading ? (
+        <div>Chargement en cours...</div>
+      ) : (
+        <div>
+          {Liste.map(pokemon => (
+            <PokemonDetail key={pokemon.name} url={pokemon.url} />
+          ))}
+          <button type="button" onClick={PagePrecedente}className='bnt_move'>Précédent</button>
+          <button type="button" onClick={PageSuivante}className='bnt_move'>Suivant</button>
+        </div>
+
+      )}
     </div>
   );
 }
-//Stockage des pokemon
-function PokemonDetail({ url }, pokemonList) {
+
+function PokemonDetail({ url }) {
   const [pokemon, setPokemon] = useState(null);
   const ls = localStorage;
 
   useEffect(() => {
-    fetch(url).then(reponse => reponse.json()).then(data => setPokemon(data)).catch(error => console.error(error));}, [url]);
-//Si pas de pokemon affichage 
+    fetch(url).then(response => response.json()).then(data => setPokemon(data)).catch(error => console.error(error));
+  }, [url]);
+//Evite les erreurs 
   if (!pokemon) {
     return <div>Chargement en cours...</div>;
   }
-
-  //permet d'ajouter des tâches dans la liste de l'utilisateur
+//Ajout au localstorage
   const ajouter = () => {
-        
-    const ls = localStorage; //sauvegarde les valeurs du Local Storage
-    ls.setItem(pokemonList, "value")//ajouter dans la localStorage de l'api
-    window.location.reload()
- }
+    ls.setItem(pokemon.name, JSON.stringify(pokemon));
+    window.location.reload();
+  };
 
   return (
-    <div className='page'>
-        <br/>
-        <span><b>Liste des Pokémons</b></span>
-
-        {Object.keys(ls).slice(0,1).map(pokemonList => (
-            <center>
-            <p className='case' pokemonList={pokemonList}>
-            <h1 >{pokemon.name}</h1>
-            <p>Numéro: {pokemon.id}</p>
-            {/* Si plusieurs type affiche tous les types du pokemon à partir du tableau */}
-            <p>Type(s): {pokemon.types.map(type => type.type.name).join(', ')}</p>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-            <br/>
-            <button className="bouton_add" onClick={ajouter} >Ajouter au Pokédex</button>
-            <br/>
-            </p>
-            </center>
-        ))}
+    <div>
+      <h1>{pokemon.name}</h1>
+      <p>Numéro: {pokemon.id}</p>
+      <p>Type(s): {pokemon.types.map(type => type.type.name).join(', ')}</p>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <br />
+      <button className="bouton_add" onClick={ajouter}>
+        Ajouter au Pokédex
+      </button>
+      <br />
+      
     </div>
   );
 }
