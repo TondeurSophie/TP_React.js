6a20e332c74292479ee5942dad9d6040f136e9a4
TondeurSophie <149052701+TondeurSophie@users.noreply.github.com>
2023-11-02 10:38:44 +0100

    mise en place de router et link

---
+++ src/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <Link></Link>
+      </div>
+    </div>
+  );
+}
+
+export default App;
